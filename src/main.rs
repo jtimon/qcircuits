a08d85f0d715056d8ed8f7a3931ce65da94bb48a
Jorge Timón <jtimon@jtimon.cc>
2021-02-28 01:26:11 +0100

    main: make repetitions an arg for test_print_hypothesis

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ use qcircuits::sources::{
     EnumParticleSource,
 };
 
-fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
+fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS, repetitions: u32) {
     let mut c_updown_single = QCircuit::new(
         Filter::new(FilterType::UpDown,
                     None,
                     None));
-    c_updown_single.run(&particle_source, 100000);
+    c_updown_single.run(&particle_source, repetitions);
     c_updown_single.print();
 
     let mut c_leftright_single = QCircuit::new(
         Filter::new(FilterType::LeftRight,
                     None,
                     None));
-    c_leftright_single.run(&particle_source, 100000);
+    c_leftright_single.run(&particle_source, repetitions);
     c_leftright_single.print();
 
     let mut c_updown_series = QCircuit::new(
@@ -33,7 +33,7 @@ fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
                                                                         None))),
                                               None))),
                     None));
-    c_updown_series.run(&particle_source, 100000);
+    c_updown_series.run(&particle_source, repetitions);
     c_updown_series.print();
 
     let mut c_leftright_series = QCircuit::new(
@@ -44,7 +44,7 @@ fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
                                                                         None))),
                                               None))),
                     None));
-    c_leftright_series.run(&particle_source, 100000);
+    c_leftright_series.run(&particle_source, repetitions);
     c_leftright_series.print();
 
     let mut c_tree2 = QCircuit::new(
@@ -56,7 +56,7 @@ fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
                                               None,
                                               None)))
         ));
-    c_tree2.run(&particle_source, 100000);
+    c_tree2.run(&particle_source, repetitions);
     c_tree2.print();
 
     let mut c_tree3 = QCircuit::new(
@@ -78,7 +78,7 @@ fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
                                                                         None)))
                     ))),
         ));
-    c_tree3.run(&particle_source, 100000);
+    c_tree3.run(&particle_source, repetitions);
     c_tree3.print();
 
     let mut c_tree4 = QCircuit::new(
@@ -116,16 +116,14 @@ fn test_print_hypothesis<PS: ParticleSource>(particle_source: PS) {
                                                                                                   None))))))
                     ))),
         ));
-    c_tree4.run(&particle_source, 100000);
+    c_tree4.run(&particle_source, repetitions);
     c_tree4.print();
 }
 
 fn main() {
-    let enum_particle_source = EnumParticleSource{};
     println!("Enum hypothesis (simplest non deterministic hypothesis):\n");
-    test_print_hypothesis(enum_particle_source);
+    test_print_hypothesis(EnumParticleSource{}, 100000);
 
-    let angle_particle_source = AngleParticleSource{};
     println!("Random angles hypothesis (non deterministic hypothesis):\n");
-    test_print_hypothesis(angle_particle_source);
+    test_print_hypothesis(AngleParticleSource{}, 100000);
 }
